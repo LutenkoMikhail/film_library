507a1c640139e50b0c3245bf13078b7beafae647
Lutenko Mikhail <lamprologus2011@gmail.com>
2021-11-16 11:50:53 +0200

    Creating project!

---
+++ config/constants.php
@@ -0,0 +1,9 @@
+<?php
+return [
+    'paginate' => [
+        'paginate_03' => 3,
+        'paginate_05' => 5,
+        'paginate_10' => 10,
+        'paginate_25' => 25,
+    ],
+];
---
+++ database/migrations/2021_11_16_173353_create_genge_films_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateGengeFilmsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('genge_films', function (Blueprint $table) {
+            $table->id();
+
+            $table->bigInteger('genre_id')->unsigned();
+            $table->bigInteger('film_id')->unsigned();
+
+            $table->foreign('genre_id')->references('id')->on('genres')->onDelete('cascade');;
+            $table->foreign('film_id')->references('id')->on('films')->onDelete('cascade');;
+
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('genge_films');
+    }
+}
